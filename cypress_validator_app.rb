d4ceb47aff84aa2a9537a608219a45a4a8fbb8ba
Michael O'Keefe <mokeefe@mitre.org>
2015-03-09 16:15:42 -0400

    Updated code to automagically pick CMS schematrons to run, and automagically figure out document type (Cat I or Cat III)

--- cypress_validator_app.rb
+++ cypress_validator_app.rb
@@ -102,7 +102,7 @@ class CypressValidatorApp < Sinatra::Base
   end
 
   post "/validate" do
-    @upload = DocumentUpload.new(params[:file][:tempfile], params[:file_type])
+    @upload = DocumentUpload.new(params[:file][:tempfile])
     erb :results
   end
 
@@ -110,11 +110,18 @@ class DocumentUpload
 
   attr_reader :errors, :doc_type, :program, :content
 
-  def initialize(file, doc_type, program=nil)
+  def initialize(file, doc_type=nil, program=nil)
     @content = File.read(file)
     @content = Nokogiri::XML(@content)
+    
+    #if the doc_type isn't passed in, see if we can find it in the document
+    doc_type = get_doc_type if !doc_type
     @doc_type = doc_type
+
+    #if the program isn't passed in, see if we can find it in the document
+    program = get_program if !program
     @program = program
+
     @errors = validators.inject([]) do |errors, v|
       errors.concat(v.validate(@content))
     end
@@ -122,6 +129,40 @@ class DocumentUpload
   end
 
   private
+
+  def get_doc_type
+    # check to see if there's a node with the templateId for a QRDA Cat I
+    if @content.at_xpath("//xmlns:templateId[@root='2.16.840.1.113883.10.20.24.1.1']")
+      return "cat1"
+    #if it's not a cat1, check to see if there's a node with the templateId for a Cat 3
+    elsif @content.at_xpath("//xmlns:templateId[@root='2.16.840.1.113883.10.20.27.1.1']")
+      return "cat3"
+    #if it's neither of these, something's probably wrong
+    else
+      raise "Document doesn't appear to be a QRDA Cat 1 or Cat 3"
+    end
+  end
+
+  def get_program 
+    #xpath for informationRecipient, which is where CMS wants the code for the program
+    prog = @content.at_xpath("//xmlns:informationRecipient/xmlns:intendedRecipient/xmlns:id/@extension")
+
+    #If it's not there, raise an error
+    if !prog
+      return nil
+    end
+
+    #Figure out if the program is EP or EH
+    return case prog.value
+    when "CPC","PQRS_MU_INDIVIDUAL","PQRS_MU_GROUP","MU_ONLY"
+      "EP"
+    when "HQR_EHR","HQR_IQR","HQR_EHR_IQR"
+      "EH"
+    else
+      #If the program name doesn't exist, return nil
+      nil
+    end
+  end
 
   def validators
     return @validators if @validators

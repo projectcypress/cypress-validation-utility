aee582fff8e0b84ce46d87169d8352d3050bd78b
Dylan Hall <dehall@mitre.org>
2016-05-11 09:31:32 -0400

    validates presence of CCN if EH

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -24,6 +24,7 @@ VALIDATOR_NAMES = {"HealthDataStandards::Validate::CDA" => "CDA",
                    "HealthDataStandards::Validate::Cat3Measure" => "Measures",
                    "HealthDataStandards::Validate::Cat3PerformanceRate" => "Performance Rate",
                    "CypressValidationUtility::Validate::Cat3PopulationValidator" => "Populations",
+                   "CypressValidationUtility::Validate::CCNValidator" => "Reporting",
                    "CypressValidationUtility::Validate::MeasurePeriodValidator" => "Measure Period"}
 
 NODE_TYPES ={ 1 => :element ,

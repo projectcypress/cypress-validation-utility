abcdcba7ab10cba74b934cf71087ac9a972ed828
Czulada, David S <dczulada@mitre.org>
2016-10-18 11:59:35 -0400

    cleaned up comment

--- lib/valueset_category_validator.rb
+++ lib/valueset_category_validator.rb
@@ -112,9 +112,8 @@ module CypressValidationUtility
           oid_tuples = @hqmf_qrda_oid_map.find_all {|map_tuple| map_tuple['qrda_oid'] == qrda_oid }
           oid_tuples.each do |oid_tuple|
             categories.each do |category|
-              # value set category is ok if it matches the hqmf type, or if vs is an attribute
+              # value set category is ok if it matches the hqmf type of the QRDA template, or if value set category is 'attribute'
               if oid_tuple['hqmf_name'].include?(CATEGORY_MAP[category]) || category == 'Attribute'
-                # if one of the template ids is the right category, true
                 return true
               end
             end

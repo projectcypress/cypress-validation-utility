fbbf5a3d816bfca6cb593fd58cac61d06ec7fce9
laurend <laurend@mitre.org>
2017-07-17 13:18:09 -0400

    commenting and rubocop cleanup

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -3,7 +3,7 @@ require 'hqmf-parser'
 
 class UploadsController < ApplicationController
   before_action :require_bundles
-  helper_method :shouldSwitchHighlight
+  helper_method :should_switch_highlight
 
   def require_bundles
     BUNDLES['2016'] ||= HealthDataStandards::CQM::Bundle.find_by(version: /^2015\./)
@@ -20,33 +20,31 @@ class UploadsController < ApplicationController
   end
 
   def create
-    begin
-      artf = Artifact.new(file: params[:file])
-      file_type = params[:file_type]
-      program = params[:program]
-      year = params[:year]
+    artf = Artifact.new(file: params[:file])
+    file_type = params[:file_type]
+    program = params[:program]
+    year = params[:year]
 
-      @upload = Upload.new(artifact: artf, file_type: file_type, 
-                           program: program, year: year)
-      @upload.save!(validate: false)
-      # TODO: rename errors on the Upload class, so we can remove this validate: false stuff
+    @upload = Upload.new(artifact: artf, file_type: file_type,
+                         program: program, year: year)
+    @upload.save!(validate: false)
+    # TODO: rename errors on the Upload class, so we can remove this validate: false stuff
 
-      FileProcessJob.perform_later(@upload.id.to_s)
+    FileProcessJob.perform_later(@upload.id.to_s)
 
-      redirect_to upload_path(@upload)
-    ensure
-      File.delete(params[:file].tempfile)
-      # rails 4 activejob adapter is not fully implemented
-      #  wrt the run at a specific time later with sucker_punch
-      # RecordCleanupJob.set(wait: 10.minutes).perform_later
-      RecordCleanupJob.perform_in(600) # run the cleanup job in 10 mins (600 sec)
-    end
+    redirect_to upload_path(@upload)
+  ensure
+    File.delete(params[:file].tempfile)
+    # rails 4 activejob adapter is not fully implemented
+    #  wrt the run at a specific time later with sucker_punch
+    # RecordCleanupJob.set(wait: 10.minutes).perform_later
+    RecordCleanupJob.perform_in(600) # run the cleanup job in 10 mins (600 sec)
   end
 
   def show
     @upload = Upload.find(params[:id])
 
-    redirect_to(root_path) and return unless @upload
+    redirect_to(root_path) && return unless @upload
     return unless @upload.completed?
 
     @bundle = BUNDLES[@upload.year]
@@ -54,27 +52,30 @@ class UploadsController < ApplicationController
     @files = @upload.qrda_files
   end
 
-  def shouldSwitchHighlight (dataCriteriaKey, populationCriteriaKey, specifics, rationale)
-    #check final specifics approach
-    shouldSwitch = false
-
-    if (specifics && specifics[populationCriteriaKey])
+  def should_switch_highlight(data_key, pop_key, specifics, rationale)
+    # check final specifics approach
+    if specifics && specifics[pop_key]
       pop_final_specifics = []
-      if (specifics[populationCriteriaKey].length > 0)
-        pop_final_specifics = specifics[populationCriteriaKey][0]
+      unless specifics[pop_key].empty?
+        # first specfics entry for this population is final specifics array
+        pop_final_specifics = specifics[pop_key][0]
       end
+      return check_specifics(data_key, rationale, pop_final_specifics)
+    end
+    false
+  end
 
-      if rationale[dataCriteriaKey][:specifics] && (rationale[dataCriteriaKey][:specifics].length >0)
-        dc_specifics = rationale[dataCriteriaKey][:specifics][0]
-
-        dc_specifics.each do |dc_spec|
-          if (dc_spec != "*") && !pop_final_specifics.include?(dc_spec)
-            return true
-          end
-        end
+  def check_specifics(data_key, rationale, pop_final_specifics)
+    if rationale[data_key][:specifics] &&
+       !rationale[data_key][:specifics].empty?
+      # get this data criteria's specific info
+      dc_specifics = rationale[data_key][:specifics][0]
+      dc_specifics.each do |dc_spec|
+        # find any data criteria specific entry that is not included
+        # in the population final specifics
+        true if (dc_spec != '*') && !pop_final_specifics.include?(dc_spec)
       end
     end
-
-    return false
+    false
   end
 end

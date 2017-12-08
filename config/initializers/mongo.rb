510210ecc4b7b3aa376f3d186760fa8ff4d28738
Czulada, David S <dczulada@mitre.org>
2017-12-08 08:34:17 -0500

    Fix Missing frozen string literal comment errors

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module QME
   patient_cache = Mongoid.default_client['patient_cache']
   patient_cache.indexes.create_one({ 'value.measure_id' => 1, 'value.sub_id' => 1, 'value.effective_date' => 1, 'value.patient_id' => 1 },

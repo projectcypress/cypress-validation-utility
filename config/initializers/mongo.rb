e7c3f002a1e693f28c71b7734691b3327f3c9fdf
Robert Clark <returntoreptar@gmail.com>
2017-12-19 15:22:00 -0500

    Added check to make sure we are running under rails server before attmpting to access the database (#66)

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 module QME
-  patient_cache = Mongoid.default_client['patient_cache']
-  patient_cache.indexes.create_one({ 'value.measure_id' => 1, 'value.sub_id' => 1, 'value.effective_date' => 1, 'value.patient_id' => 1 },
-                                   'unique' => true)
+  if defined? Rails::Server
+    patient_cache = Mongoid.default_client['patient_cache']
+    patient_cache.indexes.create_one({ 'value.measure_id' => 1, 'value.sub_id' => 1, 'value.effective_date' => 1, 'value.patient_id' => 1 },
+                                     'unique' => true)
+  end
 end

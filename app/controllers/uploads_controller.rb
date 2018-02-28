412992db3a0d092065a6cfe6ce0cae27aa86d096
laurend <laurend@mitre.org>
2018-02-28 09:10:11 -0500

    CVU sharedcypress gem integration

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'ext/artifact'
 require 'hqmf-parser'
+require 'sharedcypress'
 
 class UploadsController < ApplicationController
   before_action :require_bundles

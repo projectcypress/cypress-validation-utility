42674812a2446efc80a9239410a00e24f66c46e8
Michael O'Keefe <mokeefe@mitre.org>
2019-02-12 09:14:24 -0500

    Updated README including adding badge, and deleted unused file (#93)

--- start_cypress_validation_utility.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export TMPDIR='/home/cypress/cypress-validation-utility/cache'
-export RACK_ENV='production'
-export JAVA_OPTS='-Xmx2048m'
-cd ~cypress/cypress-validation-utility
-. /usr/local/rvm/scripts/rvm
-rvm use jruby
-torquebox deploy
-torquebox run
